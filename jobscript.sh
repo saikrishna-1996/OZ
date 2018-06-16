59b484b5ac49336edca0aec22c7cf668af92e512
Vidhi Jain <jainvidh@gra-login1.graham.sharcnet>
2018-06-16 13:54:15 -0400

    adding jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=30
 #SBATCH --mem=30000M
 #SBATCH --time=2-21:00
 #SBATCH --requeue
-#SBATCH --mail-user=saikrishnagv1996@gmail.com
+#SBATCH --mail-user=email@provider.com
 #SBATCH --mail-type=ALL
 
-source activate pytorch
+source activate sai
 echo Running on $HOSTNAME
 python launch_script.py
